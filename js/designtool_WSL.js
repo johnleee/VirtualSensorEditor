3f0a49d819a37fea8245db6bb2fd5260b5a5c2f2
sagarbhosale <sagarbhosale019@gmail.com>
2013-10-31 20:55:57 -0700

    Added From and To Date Timestamp

--- js/designtool_WSL.js
+++ js/designtool_WSL.js
@@ -37,6 +37,16 @@ function initPhysicalSensor() {
 
 function sensorTimer(sensorType) {
 
+    // this tells us if dates are filled
+    var are_dates_given = false;
+
+    if ( $("#date_from").datepicker( "getDate" ) != null && $("#date_to").datepicker( "getDate" ) != null )
+    {
+        console.log($("#date_from_alternate").val());
+        console.log($("#date_to_alternate").val());
+        are_dates_given = true;
+    }
+
     if (using_fake_data) {
       for (var deviceID in globalPhysicalSensorData) {
         globalPhysicalSensorData[deviceID][sensorType]["value"] = Math.random() * 100;
@@ -45,19 +55,34 @@ function sensorTimer(sensorType) {
       return;
     }
 
-    var urlString = hostname + "/lastest_readings_from_all_devices/" + sensorType + "/json";
-    $.get(urlString, function (newData) {
-        //if (newData == "no reading found")
-        //    return;
-        //globalPhysicalSensorData[sensorType] = JSON.parse(newData);
-        var jsonData = JSON.parse(newData);
-        for (var i = 0; i < jsonData.length; i++) {
-            var deviceID = jsonData[i]["device_id"];
-            if (globalPhysicalSensorData[deviceID] == undefined)
-                continue;
-            globalPhysicalSensorData[deviceID][sensorType]["value"] = jsonData[i]["value"];
-            globalPhysicalSensorData[deviceID][sensorType]["timestamp"] = jsonData[i]["timestamp"];
-        }
-    });
-}
+    // this is the list of device_ids set in designtool_PRE.js
+    // should we query this?
+    var device_ids = Object.keys(idReadableMapping);
 
+    // send a new query to get data per device_id.
+    for(var device_index = 0; device_index < device_ids.length; device_index++)
+    {
+        if(are_dates_given)
+        {
+            var urlString = hostname + "/sensors/" + device_ids[device_index] + "/" + $("#date_from_alternate").val() + "/" + $("#date_to_alternate").val() + "/" + sensorType + "/json";
+        }
+        else
+        {
+            var urlString = hostname + "/lastest_readings_from_all_devices/" + sensorType + "/json";
+        }
+        $.get(urlString, function (newData) {
+            //if (newData == "no reading found")
+            //    return;
+            //globalPhysicalSensorData[sensorType] = JSON.parse(newData);
+            var jsonData = JSON.parse(newData);
+            for (var i = 0; i < jsonData.length; i++) {
+                var deviceID = jsonData[i]["device_id"];
+                if (globalPhysicalSensorData[deviceID] == undefined)
+                    continue;
+                globalPhysicalSensorData[deviceID][sensorType]["value"] = jsonData[i]["value"];
+                globalPhysicalSensorData[deviceID][sensorType]["timestamp"] = jsonData[i]["timestamp"];
+            }
+        });
+    }
+
+}
